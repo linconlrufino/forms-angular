4581dea596c719a43b93601bad2f2ef68338e7d9
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-04-29 20:25:11 -0300

    Diretiva de validação

--- src/app/directives/maior-idade.directive.ts
+++ src/app/directives/maior-idade.directive.ts
@@ -1,10 +1,23 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
-  selector: '[appMaiorIdade]'
+  selector: '[maiorIdadeValidator]',
+  providers: [{
+      provide: NG_VALIDATORS,
+      useExisting: MaiorIdadeDirective,
+      multi: true
+  }]
 })
-export class MaiorIdadeDirective {
+export class  MaiorIdadeDirective implements Validator {
 
   constructor() { }
+  validate(control: AbstractControl): ValidationErrors | null {
+    const anoNascimento = new Date(control.value).getFullYear();
+    const anoAtual = new Date().getFullYear();
 
+    const maiorDeIdade = anoNascimento + 18 >= anoAtual;
+
+    return maiorDeIdade ? null : {'maiorIdadeValidator' : true };  
+  }
 }
